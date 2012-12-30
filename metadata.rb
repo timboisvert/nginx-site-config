c69368083e760e49b7fe3e56037c8cc5a293f3ba
Tim Boisvert <tboisvert@asheavenue.com>
2012-12-30 17:43:48 -0500

    added metadata

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+maintainer        "Tim Boisvert"
+maintainer_email  "tboisvert@asheavenue.com"
+license           "Apache 2.0"
+description       "Adds an nginx config file"
+version           "0.0.1"
+
+depends "nginx"
+
+%w{ debian ubuntu centos redhat fedora }.each do |os|
+  supports os
+end
+
+recipe "nginx-site-config", "Installs nginx site config"
