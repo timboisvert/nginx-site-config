a11532902b8ef96a82d273cd917b53978e39ab3a
Tim Boisvert <tboisvert@asheavenue.com>
2012-12-30 18:28:10 -0500

    changed name to site.conf

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,9 +2,9 @@ include_recipe "nginx"
 
 app_config = node["nginx-site-config"]
 
-nginx_site "default" do
+nginx_site "site" do
   server_name app_config['server_name']
   docroot app_config['docroot']
-  template "nginx/default.erb"
+  template "nginx/site.conf.erb"
   action :enable
 end
