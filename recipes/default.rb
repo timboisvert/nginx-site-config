40615402813b3fc80ce2f228630057acfc53f0bc
Tim Boisvert <tboisvert@asheavenue.com>
2012-12-30 20:55:37 -0500

    changed site to default

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,12 @@
 include_recipe "nginx"
 
-template "#{node[:nginx][:dir]}/sites-available/#{node["nginx-site-config"]["key"]}" do
+template "#{node[:nginx][:dir]}/sites-available/default" do
   source "site.conf.erb"
   owner "root"
   group "root"
   mode "0644"
 end
 
-nginx_site "#{node["nginx-site-config"]["key"]}" do
+nginx_site "default" do
   action :enable
 end
