828936e75b5a8aa94196b7171690067db33d6d9f
Tim Boisvert <tboisvert@asheavenue.com>
2012-12-30 19:55:30 -0500

    updated variable calls

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,10 +5,6 @@ template "#{node[:nginx][:dir]}/sites-available/#{node["nginx-site-config"]["key
   owner "root"
   group "root"
   mode "0644"
-  variables(
-    :docroot => node["nginx-site-config"]["docroot"],
-    :server_name => node["nginx-site-config"]['server_name']
-  )
 end
 
 nginx_site "#{node[:nginx][:dir]}/sites-available/#{node["nginx-site-config"]["key"]}" do
