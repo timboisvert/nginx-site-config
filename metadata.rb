58b3aef815f61ef9387f4b2cf526da09d9500377
Tim Boisvert <tboisvert@asheavenue.com>
2012-12-30 20:32:12 -0500

    rev version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Tim Boisvert"
 maintainer_email  "tboisvert@asheavenue.com"
 license           "Apache 2.0"
 description       "Adds an nginx config file"
-version           "0.0.1"
+version           "1.0.0"
 
 depends "nginx"
 
